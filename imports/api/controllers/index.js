fae006227fd7051444c50f4dafdace011a4d8aec
Lionels36 <85990356+Lionels36@users.noreply.github.com>
2021-11-19 15:10:15 +0100

    fix:#11

---
+++ imports/api/controllers/index.js
@@ -0,0 +1,2 @@
+import './thingControllers.js'
+import './linkControllers.js'
---
+++ imports/api/controllers/thingControllers.js
@@ -0,0 +1,54 @@
+import { Meteor } from 'meteor/meteor';
+
+import ThingModel from "../models/thingsModel";
+
+Meteor.methods({
+    //create method for create thing
+    async insertThing(name, description, reserved, serial_number, parkingType = null, hasCable, hasWhiteBoard, timestamp) {
+        try {
+
+            let result = null
+
+            console.log(name, description, reserved, serial_number, parkingType = null, hasCable, hasWhiteBoard, timestamp)
+
+            //result = ThingsModel.insertThing(name, description , reserved, serial_number, parkingType = null, hasCable, hasWhiteBoard, timestamp)
+
+            return result
+
+        } catch (e) {
+            console.error('[ThingsController][insertThing] An error occurred', e)
+            throw new Meteor.Error(e.error, e.reason, e.details)
+        }
+    },
+    //create method to delete thing
+    async removeThing(_id) {
+        try {
+            let result = null
+
+            console.log(_id)
+
+            //result = ThingsModel.removeThing(_id)
+
+            return result
+        } catch (e) {
+            console.error('[ThingsController][removeThing] An error occurred', e)
+            throw new Meteor.Error(e.error, e.reason, e.details)
+        }
+    },
+    // create method to modify thing
+    async updateThing(_id, name, description, reserved, serial_number, parkingType = null, hasCable, hasWhiteBoard, timestamp) {
+        try {
+            let result = null
+
+            console.log(_id, name, description, reserved, serial_number, parkingType = null, hasCable, hasWhiteBoard, timestamp)
+
+            //result = ThingsModel.updateThing(name, description , reserved, serial_number, parkingType = null, hasCable, hasWhiteBoard, timestamp)
+
+            return result
+        } catch (e) {
+            console.error('[ThingsController][updateThing] An error occurred', e)
+            throw new Meteor.Error(e.error, e.reason, e.details)
+        }
+    }
+
+});
