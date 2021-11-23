296628aa1e5d647c5cb1dc8e3aa7d0d58d2fbbd5
Lionels36 <85990356+Lionels36@users.noreply.github.com>
2021-11-23 11:41:53 +0100

    #12 createMethod-->category

---
+++ imports/api/controllers/categoryControllers.js
@@ -0,0 +1,54 @@
+import { Meteor } from 'meteor/meteor';
+
+import CategoriesModel from "../models/categoriesModel";
+
+Meteor.methods({
+    //create method for create categories
+    async insertCategory(title) {
+        try {
+
+            let result = null
+
+            console.log(title)
+
+            //result = CategoriesModel.insertCategory(name)
+
+            return result
+
+        } catch (e) {
+            console.error('[CategoriesController][insertCategory] An error occurred', e)
+            throw new Meteor.Error(e.error, e.reason, e.details)
+        }
+    },
+    //create method to delete Category
+    async removeCategory(_id) {
+        try {
+            let result = null
+
+            console.log(_id)
+
+            result = CategoriesModel.removeCategories(_id)
+
+            return result
+        } catch (e) {
+            console.error('[CategoriesController][removeCategory] An error occurred', e)
+            throw new Meteor.Error(e.error, e.reason, e.details)
+        }
+    },
+    // create method to modify thing
+    async updateCategory(_id, name) {
+        try {
+            let result = null
+
+            console.log(_id, name)
+
+            result = CategoriesModel.updateCategory(name)
+
+            return result
+        } catch (e) {
+            console.error('[CategoriesController][updateCategory] An error occurred', e)
+            throw new Meteor.Error(e.error, e.reason, e.details)
+        }
+    }
+
+});
