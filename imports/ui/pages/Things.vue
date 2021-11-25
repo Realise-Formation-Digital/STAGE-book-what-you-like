683127a409ef90949ebe5deafd00300494464889
Marco Tribuzio <marco.tribuz@gmail.com>
2021-11-25 10:25:29 +0100

    Correction

--- imports/ui/pages/Things.vue
+++ imports/ui/pages/Things.vue
@@ -1,95 +1,113 @@
 <template>
-  <div align="left" justify="space-around">
-    <!-- Boutton qui affiche le dialogue  -->
-    <v-btn x-large depressed class="btn-add" @click="onclickAdd()"> Add </v-btn>
 
-    <v-card flat>
-      <v-card-title>
-        <v-spacer></v-spacer>
+  <v-container>
+    <!-- button insert -->
+    <v-row>
+      <v-col>
+        <v-btn
+            large
+            rounded
+            color="green darken-1 white--text"
+            @click="onclickAdd()">
+          <v-icon left>mdi-plus</v-icon>
+          Insert Thing
+        </v-btn>
+      </v-col>
+    </v-row>
+
+    <!-- search -->
+    <v-row>
+      <v-col></v-col>
+      <v-col>
         <v-text-field
-          class="search-padding"
-          v-model="search"
-          append-icon="mdi-magnify"
-          label="Search"
-          single-line
-          hide-details
+            class="search-padding"
+            v-model="search"
+            append-icon="mdi-magnify"
+            label="Search"
+            single-line
+            hide-details
         ></v-text-field>
-      </v-card-title>
-      <v-data-table
-        class="margin-left margin-right"
-        :headers="headers"
-        :items="reservable"
-        :search="search"
-      ></v-data-table>
-    </v-card>
+      </v-col>
+    </v-row>
 
-    <v-row justify="center">
-      <v-dialog v-model="showAdd" max-width="600px">
-        <v-card>
-          <v-form ref="form" v-model="valid" lazy-validation handleSubmit>
-            <v-card-title>
-              <span class="text-h5">Add</span>
-            </v-card-title>
-            <v-card-text>
-              <v-container>
-                <v-row>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field
+    <!-- table -->
+    <v-row>
+      <v-col>
+        <v-data-table
+            class="margin-left margin-right"
+            :headers="headers"
+            :items="reservable"
+            :search="search"
+        ></v-data-table>
+      </v-col>
+    </v-row>
+
+    <!-- modal -->
+    <v-dialog v-model="showAdd" max-width="600px">
+      <v-card>
+        <v-form ref="form" v-model="valid" lazy-validation handleSubmit>
+          <v-card-title>
+            <span class="text-h5">Add</span>
+          </v-card-title>
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field
                       v-model="form.name"
                       :rules="nameRules"
                       label="Name"
                       required
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12">
-                    <v-text-field
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <v-text-field
                       v-model="form.descriptions"
                       :rules="descriptionsRules"
                       label="Descriptions"
                       required
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12">
-                    <v-text-field
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <v-text-field
                       v-model="form.serialnumber"
                       :rules="serialnumberRules"
                       label="SerialNumber"
                       required
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12">
-                    <v-text-field
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <v-text-field
                       v-model="form.categoryid"
                       :rules="categoryidRules"
                       label="CategoryId"
                       required
-                    ></v-text-field>
-                  </v-col>
+                  ></v-text-field>
+                </v-col>
 
-                  <v-col cols="12" sm="6"> </v-col>
-                </v-row>
-              </v-container>
-            </v-card-text>
-            <v-card-actions>
-              <v-spacer></v-spacer>
+                <v-col cols="12" sm="6"> </v-col>
+              </v-row>
+            </v-container>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
 
-              <v-btn
+            <v-btn
                 :disabled="!valid"
                 color="success"
                 class="mr-4"
                 @click="validate"
-              >
-                Validate
-              </v-btn>
-              <v-btn color="error" class="mr-4" @click="reset">
-                Reset Form
-              </v-btn>
-            </v-card-actions>
-          </v-form>
-        </v-card>
-      </v-dialog>
-    </v-row>
-  </div>
+            >
+              Validate
+            </v-btn>
+            <v-btn color="error" class="mr-4" @click="reset">
+              Reset Form
+            </v-btn>
+          </v-card-actions>
+        </v-form>
+      </v-card>
+    </v-dialog>
+  </v-container>
 </template>
 
 <script>
@@ -117,7 +135,7 @@ export default {
         { text: "Serial Number", value: "serial number" },
         { text: "Descriptions", value: "string" },
         { text: "Reserved ", value: "reserved" },
-        { text: "CategoryId", value: "categoryid" },
+        { text: "CategoryId", value: "categoryId" },
       ],
       // Les reservables
       reservable: [
@@ -204,6 +222,8 @@ export default {
       categoryidRules: [(v) => !!v || "CategoryId is required"],
     };
   },
+
+  //todo it miss the service and the action button to delete a single thing
 
   methods: {
     onclickAdd() {
