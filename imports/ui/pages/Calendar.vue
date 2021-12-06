37f0747d0d3c448f83560ab341368d84f2bace31
Marco Tribuzio <marco.tribuz@gmail.com>
2021-12-06 14:29:06 +0100

    Done Reservation

--- imports/ui/pages/Calendar.vue
+++ imports/ui/pages/Calendar.vue
@@ -3,8 +3,31 @@
 </template>
 
 <script>
+import categoriesCollection from "../collections/categoriesCollection";
+import reservationsCollection from "../collections/reservationsCollection";
+import thingsCollection from "../collections/thingsCollection";
+
 export default {
-  name: "Calendar"
+  name: "Calendar",
+  meteor: {
+    $subscribe: {
+      'categoriesList': [],
+      'reservationsList': [],
+      'thingsList': []
+    },
+    categoriesList() {
+      console.log("Categories", categoriesCollection.find().fetch())
+      return categoriesCollection.find().fetch()
+    },
+    reservationsList() {
+      console.log("Reservations", reservationsCollection.find().fetch())
+      return reservationsCollection.find().fetch()
+    },
+    thingsList() {
+      console.log("Things", thingsCollection.find().fetch())
+      return thingsCollection.find().fetch()
+    }
+  },
 }
 </script>
 
